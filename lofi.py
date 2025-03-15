213fd362df5302d81a6a9a6a1aa38654bcc45234
fox-coder333 <allyyshin@gmail.com>
2025-03-15 23:10:02 +0000

    new file:   lofi.py

---
+++ lofi.py
@@ -0,0 +1,12 @@
+print("Please choose a option from 1-5 for music.\n 1 - lofi \n 2 - positive music \n 3 - angry music \n 4 - sad music \n 5 - depressed music")
+option = input()
+if option == "1":
+    print("Copy and paste into a new tab for the lofi playlist: \n https://open.spotify.com/playlist/5blAlCXRxFevowEHChd2gb")
+elif option == "2":
+    print("Copy and paste into a new tab for the positive music: \n https://open.spotify.com/playlist/78mNZrkU9KGCsjWLeieUUw")
+elif option == "3":
+    print("Copy and paste into a new tab for the angry music: \n https://open.spotify.com/playlist/1jg0Lu7AQRLr1GtFnck3zk")
+elif option == "4":
+    print("Copy and paste into a new tab for the sad music: \n https://open.spotify.com/playlist/4SNZAHQFN85uCXLqC4PA9j")
+elif option == "5":
+    print("Copy and paste into a new tab for the depressed music: \n https://open.spotify.com/playlist/0U9N6b3FLB60nvHRqbRknH")
